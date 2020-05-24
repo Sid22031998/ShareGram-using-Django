bc354b6d56c453137dff2204e79e6d5ddce4b285
Siddharth Singh <siddharthrosesforyou@gmail.com>
2020-05-24 20:17:36 +0530

    second commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from register import views as v 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', v.register, name="register"),
+    path('', include('entries.urls')),
+    path('', include('django.contrib.auth.urls')),
+]
+
